737692e191d670ce92a634b755020f286c0f9a25
Ken Charette <charettekenneth@gmail.com>
2020-05-20 12:17:33 -0400

    Fetching data for searched country and current location. Added in current time and current weather icon in CurrentLocation component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import CurrentLocation from './components/CurrentLocation'
 const App = () => {
   return (
     <div className="App">
-      Weather me timbers
       <CurrentLocation />
     </div>
   );
---
+++ src/components/CountryWeather.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CountryWeather = ({country}) => {
+    return (
+        <div>
+            {country}
+
+        </div>
+    )
+}
+
+export default CountryWeather
